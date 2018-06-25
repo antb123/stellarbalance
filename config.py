424c739f5f0600ad87dd7db9579abe79fbb38e7f
Mikael <briffaudmikael@yahoo.fr>
2018-06-25 17:17:02 +0200

    stellarbalance

---
+++ config.py
@@ -0,0 +1,10 @@
+
+# select mode : 'input' type data when excute
+#               'config' use this file
+DATE_MODE = "config"
+
+# format YYYY-MM-DD
+DATE = '2017-12-31'
+
+# Id account
+ACCOUNT_ID = 'GC2BQYBXFOVPRDH35D5HT2AFVCDGXJM5YVTAF5THFSAISYOWAJQKRESK'
